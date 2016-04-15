5bc27f325b5d09282c1f839fa13d7041193a940b
Max Tang <maxtang@HutekiMacBook-Pro.local>
2016-04-15 13:32:44 +0800

    start of the new example project AudioExamplel

---
+++ AudioExample/app/src/main/java/com/example/maxtang/audioexample/AudioPlayerActivity.java
@@ -0,0 +1,13 @@
+package com.example.maxtang.audioexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AudioPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_player);
+    }
+}
